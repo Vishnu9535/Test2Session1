40d1a383d38ecfd24d3fc125bac8fda7b13c89cf
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-02-25 03:30:24 +0000

    p3

---
+++ p3original.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter a number");
+  scanf("%d",&n);
+  return n;
+}
+void is_prime(int n)
+{
+  int i,sum=0
+  for(i=1;i<=n,i++)
+    {
+      if(n%i==0){
+        sum++;
+      }
+    }
+}
+void output(int n, int sum){
+  if(sum==2){
+    printf("number is prime");    
+  }
+  else{
+    printf("number is not prime");
+  }
+}
+int main(){
+  int n,sum;
+  n=input_number();
+  is_prime(n);
+  output(n,sum);
+  return 0;
+}
