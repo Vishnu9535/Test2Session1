208850e2a3880805deab80791921728abcfce52e
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-02-27 10:18:23 +0000

    p5

--- p5original.c
+++ p5original.c
@@ -1,25 +1,42 @@
 #include<stdio.h>
+#include<math.h>
 int input_array_size(){
   int n;
   printf("enter the number");
   scanf("%d",&n);
   return n;
 }
-void init_array(int n, int a[n]
+void init_array(int n, int a[n])
 {
-  int a[n+1];
-  for(i=2;n;i++){
-    a[i]=i;
+  for(i=2;i<=n;i++){
+    a[i]=0;
     }
 }
 void erotosthenes_sieve(int n, int a[n])
 {
-  for(int i = 2; i*i <= n; i++)
-	{
-		if(a[i] != -1)
-		{
-			for(int j = 2*i; j <=n ; j += i)
-				prime[j] = -1;
-		}
-	}
-}
+  int k=2;
+  while(k <= sqrt(n)){ 
+          for(i = 2; n >= k*i; i++){
+           a[k*i] = 1;
+           }
+          k++; 
+      }
+  void out_put(int n, int a[n])
+  {
+    for(i = 2; i <= n; i++) { 
+          if(a[i] == 0) {
+             printf("the numbers are %d\n",i);
+          }
+       }
+  }
+  int main()
+{
+  int n;
+  n=input_array_size();
+  int a[1000];
+  erotosthenes_sieve(n,a);
+  out_put(n,a);
+  return 0;
+  }
+  
+    
