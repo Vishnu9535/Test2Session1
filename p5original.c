eff35f20bb248ec198c75774412927693636dffe
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-03-28 09:06:04 +0000

    p5

--- p5original.c
+++ p5original.c
@@ -1,45 +1,42 @@
-#include<stdio.h>
-#include<math.h>
-int input_array_size(){
+#include <stdio.h>
+int input_array_size()
+{
   int n;
-  printf("enter the number");
+  printf("enter the array size");
   scanf("%d",&n);
   return n;
 }
 void init_array(int n, int a[n])
 {
-  int i;
-  for(i=2;i<=n;i++){
-    a[i]=0;
-    }
+  for(int i=0;i<n;i++)
+  {
+    a[i]=i+2;
+  }
 }
 void erotosthenes_sieve(int n, int a[n])
 {
-  int k=2,y;
-  y=sqrt(n);
-  while(k<=y){ 
-          for(int i = 2; n >= k*i; i++){
-           a[k*i] = 1;
-           }
-          k++; 
-      }
-  }
-  void out_put(int n, int a[n])
+  for(int j=0;j<n;j++)
+  for(int i=2;i<a[j];i++)
   {
-    for(int i = 2; i <= n; i++) { 
-          if(a[i] == 0) {
-             printf("the numbers are %d\n",i);
-          }
-       }
+    if(a[j]%i==0)
+    {
+      a[j]=0;
+    }
   }
-  int main()
+}
+void out_put(int n, int a[n])
 {
-  int n;
+for(int i=0;i<n;i++)
+{
+  printf("%d",a[i]);
+}
+}
+int main()
+{
+  int n,a[100];
   n=input_array_size();
-  int a[1000];
+  init_array(n,a);
   erotosthenes_sieve(n,a);
   out_put(n,a);
   return 0;
   }
-  
-    
